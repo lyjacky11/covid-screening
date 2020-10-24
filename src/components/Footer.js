345a860e0a1b5d93a582b14a95d3ba0d3fd0862b
lyjacky11 <lyjacky@live.com>
2020-10-24 11:04:15 -0400

    Change footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends Component {
         return (
             <footer className={`footer ${theme}`}>
                 <FontAwesomeIcon className={`themeBtn ${theme}`} onClick={changeTheme} icon={faAdjust} size="2x"></FontAwesomeIcon>
-                <p className={theme}>© Copyright {currentYear} - <a href="https://github.com/lyjacky11" target="_blank" rel="noopener noreferrer">Jacky Ly</a>. <br /><br /> Developed with the <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a> framework.</p>
+                <p className={theme}>© Copyright {currentYear} - <a href="https://github.com/lyjacky11" target="_blank" rel="noopener noreferrer">Jacky Ly</a>. <br /><br /> Developed with the <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React.JS</a> library.</p>
             </footer>
         );
     }
